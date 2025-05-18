25ab6c33129aad639f4c468c742a0be5f9a143fa
surveyordee <surveyordeirdre@gmail.com>
2025-05-18 23:50:14 +0100

    Update script.js
    Adding api call to test button

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 //curl "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m" 
 
 function checkWeather(){
-  alert("testing");
+alert("testing");
+
+
+const apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m";
+const outputElement = document.getElementById('output');
+
+fetch(apiUrl)
+.then(response => {
+if (!response.ok) {
+throw new Error('Network response was not ok');
+}
+return response.json();
+})
+.then(data => {
+outputElement.textContent = JSON.stringify(data, null, 2);
+})
+.catch(error => {
+console.error('Error:', error);
+});
+
 }
