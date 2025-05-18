562f1bd353365195eb402a9e33fed90f0af2e7e5
surveyordee <surveyordeirdre@gmail.com>
2025-05-18 23:40:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //curl "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m" 
 
 function checkWeather{
-  alert("testing")
+  alert("testing");
 }
