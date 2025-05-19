c31e5b18a52d327b5d58c563c17e19f3f6ba41d4
surveyordee <surveyordeirdre@gmail.com>
2025-05-19 23:03:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 //curl "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m" 
 
-function checkWeather(){
+function getWeatherForcast(){
 alert("testing");
 //const apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=[51.898, 53.2724, 53.3331]&longitude=[-8.4706, -9.051, -6.2489]&daily=temperature_2m,wind_speed_10m&forecast_days=1";
 
 const apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=51.898,53.2724,53.3331&longitude=-8.4706,-9.051,-6.2489&daily=temperature_2m_min,wind_gusts_10m_max&forecast_days=1";
-for (const element of apiUrl) {
-    alert(element)
-}
 const outputElement = document.getElementById('output');
 
+    
 fetch(apiUrl)
 .then(response => {
 if (!response.ok) {
@@ -23,5 +21,11 @@ outputElement.textContent = JSON.stringify(data, null, 2);
 .catch(error => {
 console.error('Error:', error);
 });
+}
 
+function getWeather(){
+    var getWeatherForcast = getWeatherForcast()
+    for (const element of getWeatherForcast) {
+        alert(element)
+    }
 }
