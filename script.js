f37c8d05d30319c3e59e5259bcdac1afbdbd4d70
surveyordee <surveyordeirdre@gmail.com>
2025-05-20 00:25:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ return response.json();
 })
 .then(data => {
 forecastData = JSON.stringify(data, null, 2);
-dublinWeatherElement.textContent = "Temperature is " + data[0].daily.temperature_2m_min[0] +  "C windspeed is " + .daily.wind_gusts_10m_max[0] + " km\\h"
-corkWeatherElement.textContent= "Temperature is " + data[1].daily.temperature_2m_min[0] +  "C windspeed is " + .daily.wind_gusts_10m_max[0] + " km\\h"
-galwayWeatherElement.textContent = "Temperature is " + data[2].daily.temperature_2m_min[0] +  "C windspeed is " + .daily.wind_gusts_10m_max[0] + " km\\h"
+dublinWeatherElement.textContent = "Temperature is " + data[0].daily.temperature_2m_min[0] +  "C windspeed is " + data[0].daily.wind_gusts_10m_max[0] + " km\\h"
+corkWeatherElement.textContent= "Temperature is " + data[1].daily.temperature_2m_min[0] +  "C windspeed is " + data[1].daily.wind_gusts_10m_max[0] + " km\\h"
+galwayWeatherElement.textContent = "Temperature is " + data[2].daily.temperature_2m_min[0] +  "C windspeed is " + data[2].daily.wind_gusts_10m_max[0] + " km\\h"
 for (const element of data) {
     alert(element.daily.temperature_2m_min[0]);
     alert(element.daily.wind_gusts_10m_max[0]);
