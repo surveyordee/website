3c8e0320054e734039e32ed55671a64b44870a46
surveyordee <surveyordeirdre@gmail.com>
2025-05-20 00:19:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ function checkWeather(){
 
 const apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=51.898,53.2724,53.3331&longitude=-8.4706,-9.051,-6.2489&daily=temperature_2m_min,wind_gusts_10m_max&forecast_days=1";
 const outputElement = document.getElementById('output');
+
+const dublinWeatherElement = document.getElementById('DublinWeather');
+const corkWeatherElement = document.getElementById('CorkWeather');
+const galwayWeatherElement = document.getElementById('GalwayWeather');
+
+
 var forecastData;
     
 fetch(apiUrl)
@@ -13,6 +19,9 @@ return response.json();
 })
 .then(data => {
 forecastData = JSON.stringify(data, null, 2);
+dublinWeatherElement.textContent = data[0].daily.temperature_2m_min[0] 
+corkWeatherElement.textContent= data[1].daily.temperature_2m_min[0]
+galwayWeatherElement.textContent = data[2].daily.temperature_2m_min[0]
 for (const element of data) {
     alert(element.daily.temperature_2m_min[0]);
     alert(element.daily.wind_gusts_10m_max[0]);
