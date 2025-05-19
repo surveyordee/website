5afe3680c7465b392122efceb1d2957aeeae257f
surveyordee <surveyordeirdre@gmail.com>
2025-05-19 23:18:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ alert("testing");
 
 const apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=51.898,53.2724,53.3331&longitude=-8.4706,-9.051,-6.2489&daily=temperature_2m_min,wind_gusts_10m_max&forecast_days=1";
 const outputElement = document.getElementById('output');
-
+var forecastData;
     
 fetch(apiUrl)
 .then(response => {
@@ -16,7 +16,9 @@ throw new Error('Network response was not ok');
 return response.json();
 })
 .then(data => {
+forecastData =  = JSON.stringify(data, null, 2);
 outputElement.textContent = JSON.stringify(data, null, 2);
+return forecastData;
 })
 .catch(error => {
 console.error('Error:', error);
