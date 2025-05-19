c88c5878d81aadbdff5a42782ae0756a3dc559cb
surveyordee <surveyordeirdre@gmail.com>
2025-05-20 00:27:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -22,10 +22,10 @@ forecastData = JSON.stringify(data, null, 2);
 dublinWeatherElement.textContent = "Temperature is " + data[0].daily.temperature_2m_min[0] +  "C windspeed is " + data[0].daily.wind_gusts_10m_max[0] + " km\\h"
 corkWeatherElement.textContent= "Temperature is " + data[1].daily.temperature_2m_min[0] +  "C windspeed is " + data[1].daily.wind_gusts_10m_max[0] + " km\\h"
 galwayWeatherElement.textContent = "Temperature is " + data[2].daily.temperature_2m_min[0] +  "C windspeed is " + data[2].daily.wind_gusts_10m_max[0] + " km\\h"
-for (const element of data) {
-    alert(element.daily.temperature_2m_min[0]);
-    alert(element.daily.wind_gusts_10m_max[0]);
-}
+//for (const element of data) {
+//    alert(element.daily.temperature_2m_min[0]);
+//    alert(element.daily.wind_gusts_10m_max[0]);
+//}
 //outputElement.textContent = JSON.stringify(data, null, 2);
 //return forecastData;
 })
