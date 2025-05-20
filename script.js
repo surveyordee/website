46caab53281e59138bb5ad3fb54bdfe2e8477868
surveyordee <surveyordeirdre@gmail.com>
2025-05-20 22:53:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function checkWeather(){
 const apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=51.898,53.2724,53.3331&longitude=-8.4706,-9.051,-6.2489&daily=temperature_2m_min,wind_gusts_10m_max,precipitation_probability_min&forecast_days=1";
 const outputElement = document.getElementById('output');
 
-const dublinWeatherElement = document.getElementById('DublinWeather');
+ const dublinWeatherElement = document.getElementById('DublinWeather');
 const corkWeatherElement = document.getElementById('CorkWeather');
 const galwayWeatherElement = document.getElementById('GalwayWeather');
 
@@ -20,8 +20,8 @@ return response.json();
 .then(data => {
 forecastData = JSON.stringify(data, null, 2);
 dublinWeatherElement.textContent = "Temperature is " + data[0].daily.temperature_2m_min[0] +  "C Max gust windspeed is " + data[0].daily.wind_gusts_10m_max[0] + " km\\h Chance of rain " + data[0].daily.precipitation_probability_min + " %"
-corkWeatherElement.textContent= "Temperature is " + data[1].daily.temperature_2m_min[0] +  "C Max gust windspeed is " + data[1].daily.wind_gusts_10m_max[0] + " km\\h Chance of rain" + data[1].daily.precipitation_probability_min + " %"
-galwayWeatherElement.textContent = "Temperature is " + data[2].daily.temperature_2m_min[0] +  "C Max gust windspeed is " + data[2].daily.wind_gusts_10m_max[0] + " km\\h Chance of" + data[2].daily.precipitation_probability_min + " %"
+corkWeatherElement.textContent= "Temperature is " + data[1].daily.temperature_2m_min[0] +  "C Max gust windspeed is " + data[1].daily.wind_gusts_10m_max[0] + " km\\h Chance of rain " + data[1].daily.precipitation_probability_min + " %"
+galwayWeatherElement.textContent = "Temperature is " + data[2].daily.temperature_2m_min[0] +  "C Max gust windspeed is " + data[2].daily.wind_gusts_10m_max[0] + " km\\h Chance of rain " + data[2].daily.precipitation_probability_min + " %"
 })
 .catch(error => {
 console.error('Error:', error);
