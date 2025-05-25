a622b1ec838ffd798eec0d9251af041852c479d8
surveyordee <surveyordeirdre@gmail.com>
2025-05-25 23:47:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function checkWeather(){
 //Three locations specified by latitude longitude
 // Get temperature, wind gust speed,precipitation_probability_min
 // Forecast for one day
-const apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=51.898,53.2724,53.3331&longitude=-8.4706,-9.051,-6.2489&daily=temperature_2m_min,wind_gusts_10m_max,precipitation_probability_min&forecast_days=1";
+const apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=51.898,53.2724,53.3331&longitude=-8.4706,-9.051,-6.2489&daily=temperature_2m_min,wind_gusts_10m_max,precipitation_probability_mean&forecast_days=1";
 
 //Get the three locations that we will write the forcast to
 const dublinWeatherElement = document.getElementById('DublinWeather');
@@ -23,9 +23,9 @@ return response.json();
 //write html to the dublin,cork,galway tags on the html page. using innerHTML to format the html
 //using bootstrap to format the unordered list items
 //use the index to get the data for each location
-dublinWeatherElement.innerHTML = "<ul class='list-group'><li class='list-group-item'>Temperature is " + data[0].daily.temperature_2m_min[0] + "C</li>" +"<li class='list-group-item'> Max gust windspeed is " + data[0].daily.wind_gusts_10m_max[0] + " km\\h</li>"+ "<li class='list-group-item'>Chance of rain " + data[0].daily.precipitation_probability_min + " %</li></ul>"
-corkWeatherElement.innerHTML = "<ul class='list-group'><li class='list-group-item'>Temperature is " + data[1].daily.temperature_2m_min[0] +  "C</li>" +"<li class='list-group-item'> Max gust windspeed is " + data[1].daily.wind_gusts_10m_max[0] + " km\\h</li>"+ "<li class='list-group-item'>Chance of rain " + data[1].daily.precipitation_probability_min + " %</li></ul>"
-galwayWeatherElement.innerHTML = "<ul class='list-group'><li class='list-group-item'>Temperature is " + data[2].daily.temperature_2m_min[0] +  "C</li>" +"<li class='list-group-item'> Max gust windspeed is " + data[2].daily.wind_gusts_10m_max[0] + " km\\h</li>"+ "<li class='list-group-item'>Chance of rain " + data[2].daily.precipitation_probability_min + " %</li></ul>"
+dublinWeatherElement.innerHTML = "<ul class='list-group'><li class='list-group-item'>Temperature is " + data[0].daily.temperature_2m_min[0] + "C</li>" +"<li class='list-group-item'> Max gust windspeed is " + data[0].daily.wind_gusts_10m_max[0] + " km\\h</li>"+ "<li class='list-group-item'>Chance of rain " + data[0].daily.precipitation_probability_mean + " %</li></ul>"
+corkWeatherElement.innerHTML = "<ul class='list-group'><li class='list-group-item'>Temperature is " + data[1].daily.temperature_2m_min[0] +  "C</li>" +"<li class='list-group-item'> Max gust windspeed is " + data[1].daily.wind_gusts_10m_max[0] + " km\\h</li>"+ "<li class='list-group-item'>Chance of rain " + data[1].daily.precipitation_probability_mean + " %</li></ul>"
+galwayWeatherElement.innerHTML = "<ul class='list-group'><li class='list-group-item'>Temperature is " + data[2].daily.temperature_2m_min[0] +  "C</li>" +"<li class='list-group-item'> Max gust windspeed is " + data[2].daily.wind_gusts_10m_max[0] + " km\\h</li>"+ "<li class='list-group-item'>Chance of rain " + data[2].daily.precipitation_probability_mean + " %</li></ul>"
 })
 .catch(error => {
  //write error to the console in the browser
